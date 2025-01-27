2d6ca6bf3e58a270b6fa74013f7d9f677310ef4e
Rafael Pinto <rafaelgcpp@gmail.com>
2025-01-27 20:52:47 -0300

    Fixing the proxy definition

--- DynSec.Web/src/main.ts
+++ DynSec.Web/src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { MainComponent } from './main/main.component';
+import { mainConfig } from './main/main.config';
+
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+//bootstrapApplication(MainComponent, mainConfig)
+//  .catch((err) => console.error(err));
