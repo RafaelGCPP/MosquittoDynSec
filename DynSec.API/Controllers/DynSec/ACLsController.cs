94b78b5fc437596c774006a8a8d5db9f0be7bc83
Rafael Pinto <rafaelgcpp@gmail.com>
2025-01-28 21:33:50 -0300

    implementing an ACL controller

---
+++ DynSec.API/Controllers/DynSec/ACLsController.cs
@@ -0,0 +1,35 @@
+﻿using DynSec.Model.Responses;
+using DynSec.Protocol.Exceptions;
+using DynSec.Protocol.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynSec.API.Controllers.DynSec
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ACLsController : ControllerBase
+    {
+        private readonly IACLService aclsService;
+
+        public ACLsController(IACLService _aclsService) { aclsService = _aclsService; }
+
+        // GET: api/<MQTTdynsecController>/defaultACL
+        [HttpGet("defaultACL")]
+        public async Task<ActionResult<DefaultACLAccessData>> GetRoles()
+        {
+            try
+            {
+                return Ok(await aclsService.GetDefault());
+            }
+            catch (DynSecProtocolTimeoutException e)
+            {
+                return StatusCode(504, e.Message);
+            }
+            catch (DynSecProtocolNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+    }
+}
+
