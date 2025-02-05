65694835af7cb7971bd0a84f34230c0422ee588b
Rafael Pinto <rafaelgcpp@gmail.com>
2025-02-05 20:51:56 -0300

    Implementing sidenav functionality

---
+++ DynSec.Web/src/app/clients/clients.component.ts
@@ -0,0 +1,27 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { NavBarService } from '../navbar/navbar.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'dynsec-clients',
+  imports: [MatSidenavModule, MatButtonModule],
+  templateUrl: './clients.component.html',
+  styleUrl: './clients.component.scss'
+})
+export class ClientsComponent {
+  showFiller = false;
+  navbarSubscription!: Subscription;
+  @ViewChild('drawer') sidenav!: MatSidenav;
+
+  constructor(private readonly navbarSvc: NavBarService) { }
+
+  ngAfterViewInit() {
+    this.navbarSubscription = this.navbarSvc.subscribe(this.sidenav);
+  }
+
+  ngOnDestroy() {
+    this.navbarSubscription.unsubscribe();
+  }
+}
--- DynSec.Web/src/app/navbar/navbar.component.ts
+++ DynSec.Web/src/app/navbar/navbar.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { Subscription } from 'rxjs';
+import { NavBarService } from './navbar.service';
+
 
 @Component({
   selector: 'dynsec-navbar',
   imports: [MatButtonModule, MatIconModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  svcSubscription!: Subscription;
+
+  constructor(private readonly navbarSvc: NavBarService) {
+  }
+
+  ngOnInit() {
+    this.svcSubscription=this.navbarSvc.showSidenav.subscribe(show => {
+      console.log("Show Sidenav: ", show);
+    });
+  }
+
+  toggleSidenav() {
+    this.navbarSvc.toggleSidenav();
+  }
+
+  ngOnDestroy() {
+    this.svcSubscription.unsubscribe();
+  }
 
 }
