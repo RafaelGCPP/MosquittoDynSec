9b599e885c2fd3bff99970c079ad537d9fc213f2
Rafael Pinto <rafaelgcpp@gmail.com>
2025-02-06 14:40:52 -0300

    migrating from custom handler to default RPC

---
+++ DynSec.Protocol/DynamicSecurityRpc.cs
@@ -0,0 +1,29 @@
+﻿using DynSec.Model;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MQTTnet;
+using MQTTnet.Extensions.Rpc;
+using MQTTnet.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynSec.Protocol
+{
+    public class DynamicSecurityRpc
+    {
+        private readonly IMqttClient client;
+        private readonly MqttClientOptions options;
+        private readonly ILogger<DynamicSecurityHandler> logger;
+
+        public DynamicSecurityRpc(IMqttClient mqttClient, MqttClientOptions? mqttOptions, ILogger<DynamicSecurityHandler> _logger)
+        {
+            client = mqttClient;
+            options = mqttOptions ?? new();
+            logger = _logger;
+
+        }
+    }
+}
