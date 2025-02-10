29438ab8c241a86ba653608a576078b636b9fd14
Rafael Pinto (Y1RY) <rafael.gustavo@petrobras.com.br>
2025-02-10 17:05:59 -0300

    Renaming component

---
+++ DynSec.Web/src/app/client-detail/client-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dynsec-client-detail',
+  imports: [],
+  templateUrl: './client-detail.component.html',
+  styleUrl: './client-detail.component.scss'
+})
+export class ClientDetailComponent {
+
+}
