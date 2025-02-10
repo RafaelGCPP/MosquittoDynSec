70777d3db429f429fd74e158c4037d983b01c047
Rafael Pinto (Y1RY) <rafael.gustavo@petrobras.com.br>
2025-02-10 16:56:10 -0300

    Creating client view

---
+++ DynSec.Web/src/app/client-view/client-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dynsec-client-view',
+  imports: [],
+  templateUrl: './client-view.component.html',
+  styleUrl: './client-view.component.scss'
+})
+export class ClientViewComponent {
+
+}
