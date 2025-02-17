5a3cbf50c2e0c679d158cd002e6c314d93659116
Rafael Pinto <rafaelgcpp@gmail.com>
2025-02-17 08:02:56 -0300

    adjusting codes doing proper error treatment on graphql server

---
+++ DynSec.Protocol/Exceptions/DynSecProtocolDuplicatedException.cs
@@ -0,0 +1,18 @@
+﻿namespace DynSec.Protocol.Exceptions
+{
+    [Serializable]
+    public class DynSecProtocolDuplicatedException : DynSecProtocolException
+    {
+        public DynSecProtocolDuplicatedException()
+        {
+        }
+
+        public DynSecProtocolDuplicatedException(string? message) : base(message)
+        {
+        }
+
+        public DynSecProtocolDuplicatedException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
