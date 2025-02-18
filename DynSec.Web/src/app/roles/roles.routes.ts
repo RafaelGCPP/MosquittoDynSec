069f1c95d364e9436eb024a8641459a979f76803
Rafael Pinto <rafaelgcpp@gmail.com>
2025-02-18 18:29:42 -0300

    fixing CSS

--- DynSec.Web/src/app/roles/roles.routes.ts
+++ DynSec.Web/src/app/roles/roles.routes.ts
@@ -5,6 +5,9 @@ export const routes: Routes = [
     path: '', loadComponent: () => import('./roles.component').then(m => m.RolesComponent),
     children: [
       {
+        path: 'new', loadComponent: () => import('./role-detail/role-detail.component').then(m => m.RoleDetailComponent),
+      },
+      {        
         path: ':roleName', loadComponent: () => import('./role-detail/role-detail.component').then(m => m.RoleDetailComponent),
       },
     ]
