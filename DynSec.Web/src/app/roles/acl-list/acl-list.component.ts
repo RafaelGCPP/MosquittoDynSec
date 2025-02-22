17fa9d8a10a7be78ce2b9bcaca6c10d927805f05
Rafael Pinto <rafaelgcpp@gmail.com>
2025-02-22 07:19:14 -0300

    finishing acl-list component.

--- DynSec.Web/src/app/roles/acl-list/acl-list.component.ts
+++ DynSec.Web/src/app/roles/acl-list/acl-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, model } from '@angular/core';
+import { Component, model, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -28,8 +28,35 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 export class AclListComponent {
   displayedColumns = ['permission','topic', 'allow', 'priority', 'buttons'];
   selectedValues = model<Acl[]>([]);
+  modified = output<void>();
+  
   aclTypes = Object.values(ACLtype);
+  newValue = {
+    aclType: ACLtype.subscribe,
+    topic: '#',
+    allow: 'true',
+    priority: 0
+  };
 
   addItem() {
+    const acl: Acl = {
+      aclType: this.newValue.aclType,
+      topic: this.newValue.topic,
+      allow: this.newValue.allow === 'true',
+      priority: this.newValue.priority
+    };
+    this.selectedValues.update((acls) => [...acls, acl]);
+  }
+
+  removeItem(acl: Acl) {
+    this.newValue = {
+      aclType: acl.aclType,
+      topic: acl.topic,
+      allow: acl.allow ? 'true' : 'false',
+      priority: acl.priority
+    }
+    
+    this.selectedValues.update((acls)=> acls.filter(a => a !== acl));
+   
   }
 }
