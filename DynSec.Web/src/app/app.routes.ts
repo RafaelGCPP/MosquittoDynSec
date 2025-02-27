168a7b16c41b358b7d646936f6cac89c6f00a4a4
Rafael Pinto <rafaelgcpp@gmail.com>
2025-02-27 16:31:20 -0300

    Add HomePageComponent and update routing
    - Updated `app.routes.ts` to include `HomePageComponent` for unmatched routes.
    - Modified `home-page.component.html` to add a structured layout with titles.
    - Created/updated `home-page.component.scss` for styling the home page.
    - Defined `HomePageComponent` in `home-page.component.ts` with necessary metadata.

--- DynSec.Web/src/app/app.routes.ts
+++ DynSec.Web/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
   { path: 'clients', loadChildren: () => import('./clients/clients.routes').then(m => m.routes) },
   { path: 'roles', loadChildren: () => import('./roles/roles.routes').then(m => m.routes) },
   { path: 'groups', loadChildren: () => import('./groups/groups.routes').then(m => m.routes) },
   { path: 'default-acl', loadChildren: () => import('./default-acl/default-acl.routes').then(m => m.routes) },
-
+  { path: '**', component: HomePageComponent }
 ];
