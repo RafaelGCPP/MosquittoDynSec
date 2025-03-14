8929cf9a655bdbff37ac00b8ce9fc5a2a1a732ee
Rafael Pinto <rafaelgcpp@gmail.com>
2025-03-14 08:26:44 -0300

    Update project references and modify application setup
    - Added new package references for ApiDocs, AppHost, and NodeJs.
    - Included additional project references for ServiceDefaults, GraphQL, Model, MQTT, and Protocol.
    - Updated Program.cs to include a new using directive and modified application setup by commenting out the frontend configuration and adding a new environment variable.

--- DynSec.Aspire/Program.cs
+++ DynSec.Aspire/Program.cs
@@ -1,17 +1,22 @@
+using Arshid.Aspire.ApiDocs.Extensions;
 using Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 var dynsecapi = builder.AddProject<Projects.DynSec_API>("DynSecApi")
-    .WithExternalHttpEndpoints();
-
-var dynsecfrontend=builder.AddNpmApp("angular", "..\\DynSec.Web")
-    .WithReference(dynsecapi)
-    .WithHttpsEndpoint(port:4200, targetPort:4200,name:"frontend",env: "PORT", isProxied:false)
-    .WithEnvironment("ASPNETCORE_HTTPS_PORT","7044")
+    .WithEnvironment("ASPNETCORE_HTTPS_PORT", "7044")
+    .WithEnvironment("FRONTEND_HTTPS_PORT", "4200")
     .WithExternalHttpEndpoints()
-    .WaitFor(dynsecapi)
-    ;
+    .WithScalar();
+
+
+//var dynsecfrontend=builder.AddNpmApp("angular", "..\\DynSec.Web")
+//    .WithReference(dynsecapi)
+//    .WithHttpsEndpoint(port:4200, targetPort:4200,name:"frontend",env: "PORT", isProxied:false)
+//    .WithEnvironment("ASPNETCORE_HTTPS_PORT","7044")
+//    .WithExternalHttpEndpoints()
+//    .WaitFor(dynsecapi)
+//    ;
 
 
 builder.Build().Run();
